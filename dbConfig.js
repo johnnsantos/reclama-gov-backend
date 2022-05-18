1c3d4d13795a9efca8d85209fdaa01786640b979
Johnnatan <contatojohnnatan@gmail.com>
2022-05-17 22:29:44 -0300

    feat: migrating to postgres for use heroku

---
+++ dbConfig.js
@@ -0,0 +1,4 @@
+"use strict";
+const dbEngine = process.env.DB_ENVIORMENT || "development";
+const config = require(`./knexfile.${dbEngine}`);
+module.exports = require('knex')(config);
---
+++ knexfile.js
@@ -0,0 +1,27 @@
+"use strict";
+module.exports = {
+    development: {
+        client: 'sqlite3',
+        useNullAsDefault: true,
+        connection: {
+            filename: './data/dev.sqlite3'
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run('PRAGMA foreign_keys = ON', done);
+            },
+        },
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: './data/migrations',
+        }
+    }
+};
