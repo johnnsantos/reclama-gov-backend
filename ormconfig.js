04ba953049f5f042d14c2e370969b837facd289d
Johnnatan <contatojohnnatan@gmail.com>
2022-05-17 23:07:30 -0300

    feat: migrating to postgres for use heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,9 @@
 module.exports ={
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "options": {
+    "ssl": true
+  },
   "entities": ["src/entity/**/*.ts"],
   "migrations": ["src/migration/**/*.ts"],
   "subscribers": ["src/subscriber/**/*.ts"],
