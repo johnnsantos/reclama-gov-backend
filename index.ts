65b1dfa6a150ccdf7d14d3cb5aaa5b061991d24f
Johnnatan <contatojohnnatan@gmail.com>
2022-05-25 21:28:37 -0300

    fix: preventing app crash with try catch

--- index.ts
+++ index.ts
@@ -3,13 +3,15 @@ import "reflect-metadata";
 require("dotenv").config();
 import express from "express";
 import cors from "cors";
-
+const bodyParser = require("body-parser");
 import "./src/database";
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post("/requirements", RequirementController.saveNewRequirement);
 app.get("/requirements", RequirementController.getAllRequirements);
--- src/api/RequirementController.ts
+++ src/api/RequirementController.ts
@@ -3,13 +3,28 @@ import { getRepository } from "typeorm";
 import { Requirement } from "../entity/Requirement";
 
 export const saveNewRequirement = async (req: Request, res: Response) => {
-  const requirementsRepository = getRepository(Requirement);
-  const savedRequirement = await requirementsRepository.save(req.body);
-  return res.json(savedRequirement);
+  try {
+    const requirementsRepository = getRepository(Requirement);
+    const newRequirement = new Requirement();
+    newRequirement.local_name = req.body.local_name;
+    newRequirement.description = req.body.description;
+    newRequirement.category = req.body.category;
+    newRequirement.contact = req.body.contact;
+    newRequirement.lat = req.body.lat;
+    newRequirement.long = req.body.long;
+    await requirementsRepository.save(newRequirement);
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 };
 
 export const getAllRequirements = async (req: Request, res: Response) => {
-  const requirementsRepository = getRepository(Requirement);
-  const requirements = await requirementsRepository.find();
-  return res.json(requirements);
+  try {
+    const requirementsRepository = getRepository(Requirement);
+    const requirements = await requirementsRepository.find();
+    return res.json(requirements);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 };
